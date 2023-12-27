8b5d48067a02aed66d9be931bd2df84ee07b71f3
Yigit99 <154807428+Yigit99@users.noreply.github.com>
2023-12-27 14:10:46 +0300

    Services Files Upload

---
+++ oyunprojesideneme2.Services/ProductClass.cs
@@ -0,0 +1,57 @@
+﻿using ooyunsitesiprojedeneme2.domain.Collections.Interfaces;
+using ooyunsitesiprojedeneme2.Domain;
+using oyunsitesiprojedeneme2.Domain.Collections;
+using oyunsitesiprojedeneme2.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oyunprojesideneme2.Services
+{
+  
+        public class ProductManager 
+    { 
+        
+            private IProductManager _manager;
+            public ProductManager(IProductManager product)
+            {
+                _manager = product;
+            }
+            public List<BucketDataModel> bucetGetProductFromDatabase(int pageNumber, List<int> cartIds) {
+
+
+            List<BucketDataModel> products = new List<BucketDataModel>();
+
+            ProductsCollection productManager = new ProductsCollection();
+            products=productManager.GetBuckets(pageNumber, cartIds);
+            return products;
+        }
+
+            public void productAdd(ProductsDataModel data)
+            {
+            ProductsCollection productCollecCon = new ProductsCollection();
+            productCollecCon.Add(data);
+            }
+
+            public void productRemove(string removeName)
+            {
+            ProductsCollection productCollecCon = new ProductsCollection();
+            productCollecCon.RemoveProduct(removeName);
+            }
+
+            public bool productControl(ProductsDataModel data) { 
+            ProductsCollection productsCollecCon = new ProductsCollection();
+            return productsCollecCon.productControl(data);
+        }
+            
+            public List<ProductsDataModel> GetProducts(string pageNumber) { 
+        
+            ProductsCollection productsCollection = new ProductsCollection();
+            return productsCollection.GetProductsFromDatabase(pageNumber);
+
+        }
+    }
+}
---
+++ oyunprojesideneme2.Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using ooyunsitesiprojedeneme2.domain.Collections.Interfaces;
+using ooyunsitesiprojedeneme2.Domain;
+using oyunsitesiprojedeneme2.Domain.Collections;
+using oyunsitesiprojedeneme2.Domain.Models;
+using System.Diagnostics.SymbolStore;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace oyunprojesideneme2.Services
+{
+
+    public class customerManager {
+
+        private IRepository _manager;
+        public customerManager(IRepository product)
+        {
+            _manager = product;
+        }
+        //-----------------------------------------------------
+        public void registerSaving(UsersDataModel aga) {
+
+            _manager.Add(aga);
+
+            }
+        public void usersRemove(UsersDataModel aga)
+        {
+
+            _manager.usersRemove(aga);
+
+        }
+        public bool Login(UsersDataModel aga)
+        { 
+        
+            return _manager.Login(aga);
+
+        }
+        //-----------------------------------------------------
+    }
+}
